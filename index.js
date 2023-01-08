ccfe2d1b3f5c04e523b6e5b02bfdf678f80c92dc
JAMES <jameskibunja17@gmail.com>
2023-01-08 14:23:17 +0300

    Completed Word

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names=['Guadalupe','Ollie','Aki'];
+const word = "surprise";
+
+function writeCards( names, word ) {
+ let eachCard = []
+ for ( let i = 0; i < names.length; i++ ) {
+   eachCard.push( `Thank you, ${names[i]}, for the wonderful ${word} gift!` )
+ }
+ return eachCard
+}
+
+function countDown(){
+    let number = 11;
+    while (number --){
+        console.log(number)
+    }
+}
